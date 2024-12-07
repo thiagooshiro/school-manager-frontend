a4ebe73169e083947e57b5784bcfb6852ec29051
thiagooshiro <oshirothiago@gmail.com>
2024-12-07 20:13:37 -0300

    Changes in loayoiut

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,23 @@
+"use client"
+
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Crie a instância do QueryClient
+const queryClient = new QueryClient();
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR">
       <head />
       <body>
-        <div style={{ padding: "20px" }}>
-          <h1>School Manager</h1>
-          {children}
-        </div>
+        {/* Envolva o conteúdo da página com o QueryClientProvider */}
+        <QueryClientProvider client={queryClient}>
+          <div style={{ padding: "20px" }}>
+            <h1>School Manager</h1>
+            {children}
+          </div>
+        </QueryClientProvider>
       </body>
     </html>
   );
